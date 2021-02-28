ca31e3c4b957fecda71c57019fa3b0bf43f813d7
Jeff Wagner <jeff@moonrise.works>
2021-02-28 17:03:07 -0600

    get favorites when visiting /favorites path

--- src/containers/Favorites/Favorites.js
+++ src/containers/Favorites/Favorites.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const url = 'http://localhost:3000/local/graphql';
 
 const Favorites = () => {
-	console.log('rendering favs');
+	const [favorites, setFavorites] = useState([]);
+
+	useEffect(() => {
+		const query = `
+		{
+			getAllFavorites {
+        favorites {
+					github_id
+					owner_id
+	        owner_name
+	        url
+        }
+    	}
+		}
+	`;
+
+	
+	return fetch(url, { 
+		method: 'POST',
+		headers: {'Content-Type': 'application/graphql'},
+		body: JSON.stringify({ query })
+	})
+	.then((res) => res.json())
+	.then(data => {
+		setFavorites(data.data?.getAllFavorites?.favorites ?? []);
+	})
+	.catch( err => console.error(err));
+}, []);
+
 	return (
 		<div>
-			Favorites
+			<ul className="list-group">
+			{!favorites.length ? 'No Favorites Yet' : favorites.map( fav => {
+  				return <li key={fav.github_id} className="list-group-item">{fav.url}</li>
+			})}
+		</ul>
 		</div>
 	);
 }
